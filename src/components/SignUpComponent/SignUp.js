14ea9588e337012f6eda867d03fcc40899ae1a87
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-18 01:36:15 +0530

    Final eval

--- src/components/SignUpComponent/SignUp.js
+++ src/components/SignUpComponent/SignUp.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './SignUp.module.css';
+import signupimg from '../../assets/images/signupimg.png';
+import toast , { Toaster } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   return (
