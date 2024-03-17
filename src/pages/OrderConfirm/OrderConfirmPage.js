2b8da0d0b772a9f5b940100484c1a9a7439c740e
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-17 21:45:11 +0530

    Final eval

---
+++ src/pages/OrderConfirm/OrderConfirmPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OrderConfirm from '../../components/OrderConfirmComponent/OrderConfirm';
+
+export default function OrderConfirmPage() {
+  return (
+    <div>
+        <OrderConfirm />
+    </div>
+  )
+}
---
+++ src/pages/PlaceOrder/PlaceOrderPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlaceOrder from '../../components/PlaceOrderComponent/PlaceOrder';
+
+export default function PlaceOrderPage() {
+  return (
+    <div>
+        <PlaceOrder />
+    </div>
+  )
+}
