e3963e962cbd60cfa9c1f4d41c76fdf4c173196b
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-19 01:48:50 +0530

    Final eval

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-//const backendUrl = process.env.REACT_APP_BACKEND_URL;
-const backendUrl = 'http://localhost:7000/api/v1';
+const backendUrl = process.env.REACT_APP_BACKEND_URI;
 
 export const signupUser = async({ name, mobile , email, password }) => {
     try {
         const requestUrl = `${backendUrl}/auth/signup`;
         const requestPayload = { name, mobile , email, password };
+        console.log(requestUrl);
         const response = await axios.post(requestUrl, requestPayload);
         return response.data;
     } catch (error) {
