7abf6d13cce6670ad1840bcb8179169b861c7de1
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-26 02:58:37 +0530

    Final eval

--- src/components/HomeComponent/Home.js
+++ src/components/HomeComponent/Home.js
@@ -13,21 +13,21 @@ import { useNavigate } from 'react-router-dom';
 export default function Home() {
 
   const navigate = useNavigate();
-  const [init , setInit] = useState();
+  const [init, setInit] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    if(localStorage.getItem('username')){
+    if (localStorage.getItem('username')) {
       setInit(localStorage.getItem('username').split(" ").map((word) => word[0]).join(''));
     }
-    
-  },[]);
+
+  }, []);
 
   const openorclosepopup = (i) => {
-    if(document.getElementById(i).style.visibility == 'hidden')
-    document.getElementById(i).style.visibility = 'visible';
+    if (document.getElementById(i).style.visibility == 'hidden')
+      document.getElementById(i).style.visibility = 'visible';
     else
-    document.getElementById(i).style.visibility = 'hidden';
+      document.getElementById(i).style.visibility = 'hidden';
   }
 
   const setfeedbackchoice = (i) => {
@@ -49,20 +49,20 @@ export default function Home() {
       <div className={styles.title}>
         <img src={signupimg} className={styles.signupimg} alt='' />
         <div className={styles.musicart}>Musicart</div>
-        <div className={styles.home} onClick={()=>{navigate('/')}}>Home</div>
+        <div className={styles.home} onClick={() => { navigate('/') }}>Home</div>
         {localStorage.getItem('token') ?
-        <>
-         <div className={styles.home} onClick={()=>{navigate('/invoice')}}>Invoice</div>
-         <div className={styles.cart}>
-          <img src={whitecart} className={styles.whitecart} alt='' />
-          <div className={styles.viewcart}>View Cart&ensp;0</div>
-         </div>
-         <div className={styles.userinfo} onClick={()=>openorclosepopup('menu')}>
-            <div className={styles.initials}>{init}</div>
-         </div>
-         </> 
-         :
-         ""}
+          <>
+            <div className={styles.home} onClick={() => { navigate('/invoice') }}>Invoice</div>
+            <div className={styles.cart}>
+              <img src={whitecart} className={styles.whitecart} alt='' />
+              <div className={styles.viewcart}>View Cart&ensp;0</div>
+            </div>
+            <div className={styles.userinfo} onClick={() => openorclosepopup('menu')}>
+              <div className={styles.initials}>{init}</div>
+            </div>
+          </>
+          :
+          ""}
       </div>
       <div className={styles.ad}>
         <div className={styles.grab}>
@@ -70,13 +70,13 @@ export default function Home() {
           Selected headphones
         </div>
         {localStorage.getItem('token') ?
-        <div className={styles.usermenu} id='menu' style={{visibility:'hidden'}}>
-          <div className={styles.name}>{localStorage.getItem('username')}</div>
-          <div className={styles.line2}></div>
-          <div className={styles.logout}>Logout</div>
-        </div>
-        :
-        ""
+          <div className={styles.usermenu} id='menu' style={{ visibility: 'hidden' }}>
+            <div className={styles.name}>{localStorage.getItem('username')}</div>
+            <div className={styles.line2}></div>
+            <div className={styles.logout}>Logout</div>
+          </div>
+          :
+          ""
         }
       </div>
       <img src={bgimage} alt='' className={styles.bgimage} />
@@ -89,30 +89,30 @@ export default function Home() {
         <img src={listview} alt='' className={styles.list} />
       </div>
       {localStorage.getItem('token') ?
-      <>
-      <div className={styles.feedbackcontainer} id='feedback-container' style={{visibility:'hidden'}}>
-        <div className={styles.typeoffeedback}>Type of feedback</div>
-        <div className={styles.typeselect} onClick={()=>openorclosepopup('feedbackchoice')} >
-        <div id='choicetype' style={{width:'8vw'}}>Choose the type</div>
-        <div style={{marginLeft:'3.5vw'}}>v</div>
-        </div>
-         <div className={styles.options} style={{visibility:'hidden'}} id='feedbackchoice'>
-          <div style={{paddingLeft:'0.5vw' , paddingTop:'1vh'}} onClick={()=>setfeedbackchoice('Bugs')}>Bugs</div>
-          <div className={styles.line3}></div>
-          <div style={{paddingLeft:'0.5vw' , paddingTop:'1vh'}} onClick={()=>setfeedbackchoice('Feedback')}>Feedback</div>
-          <div className={styles.line3}></div>
-          <div style={{paddingLeft:'0.5vw' , paddingTop:'1vh'}} onClick={()=>setfeedbackchoice('Query')}>Query</div>
-         </div>
-        <div className={styles.feedback}>Feedback</div>
-        <textarea className={styles.feedbacktext} spellCheck={false} placeholder='Type your feedback'></textarea>
-        <div className={styles.submit}>Submit</div>
-      </div>
-      <div className={styles.userfeedback} onClick={()=>openorclosepopup('feedback-container')}>
-        <img src={feedback} alt='' className={styles.feedbackimage}/>
-      </div>
-      </>
-      :
-      ""
+        <>
+          <div className={styles.feedbackcontainer} id='feedback-container' style={{ visibility: 'hidden' }}>
+            <div className={styles.typeoffeedback}>Type of feedback</div>
+            <div className={styles.typeselect} onClick={() => openorclosepopup('feedbackchoice')} >
+              <div id='choicetype' style={{ width: '8vw' }}>Choose the type</div>
+              <div style={{ marginLeft: '3.5vw' }}>v</div>
+            </div>
+            <div className={styles.options} style={{ visibility: 'hidden' }} id='feedbackchoice'>
+              <div style={{ paddingLeft: '0.5vw', paddingTop: '1vh' }} onClick={() => setfeedbackchoice('Bugs')}>Bugs</div>
+              <div className={styles.line3}></div>
+              <div style={{ paddingLeft: '0.5vw', paddingTop: '1vh' }} onClick={() => setfeedbackchoice('Feedback')}>Feedback</div>
+              <div className={styles.line3}></div>
+              <div style={{ paddingLeft: '0.5vw', paddingTop: '1vh' }} onClick={() => setfeedbackchoice('Query')}>Query</div>
+            </div>
+            <div className={styles.feedback}>Feedback</div>
+            <textarea className={styles.feedbacktext} spellCheck={false} placeholder='Type your feedback'></textarea>
+            <div className={styles.submit}>Submit</div>
+          </div>
+          <div className={styles.userfeedback} onClick={() => openorclosepopup('feedback-container')}>
+            <img src={feedback} alt='' className={styles.feedbackimage} />
+          </div>
+        </>
+        :
+        ""
       }
       <div className={styles.bottom}>Musicart | All rights reserved</div>
     </>
