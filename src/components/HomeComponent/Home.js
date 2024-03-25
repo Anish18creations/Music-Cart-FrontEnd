5d0a4b59a0eeee548fb08fbed95427d87e824a01
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-25 15:19:46 +0530

    Final eval

--- src/components/HomeComponent/Home.js
+++ src/components/HomeComponent/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 import phonecall from '../../assets/images/phonecall.png';
 import signupimg from '../../assets/images/signupimg.png';
@@ -6,8 +6,22 @@ import bgimage from '../../assets/images/bgimage.png';
 import search from '../../assets/images/search.png';
 import gridview from '../../assets/images/gridview.png';
 import listview from '../../assets/images/listview.png';
+import whitecart from '../../assets/images/whitecart.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+
+  const navigate = useNavigate();
+  const [init , setInit] = useState();
+
+  useEffect(()=>{
+
+    if(localStorage.getItem('username')){
+      setInit(localStorage.getItem('username').split(" ").map((word) => word[0]).join(''));
+    }
+    
+  },[]);
+
   return (
     <>
       <div className={styles.navbar}>
@@ -22,7 +36,23 @@ export default function Home() {
       <div className={styles.title}>
         <img src={signupimg} className={styles.signupimg} alt='' />
         <div className={styles.musicart}>Musicart</div>
-        <div className={styles.home}>Home</div>
+        <div className={styles.home} onClick={()=>{navigate('/')}}>Home</div>
+        {localStorage.getItem('token') ?
+        <>
+         <div className={styles.home} onClick={()=>{navigate('/invoice')}}>Invoice</div>
+         <div className={styles.cart}>
+          <img src={whitecart} className={styles.whitecart} alt='' />
+          <div className={styles.viewcart}>View Cart&ensp;0</div>
+         </div>
+         <div className={styles.userinfo}>
+            <div className={styles.initials}>{init}</div>
+         </div>
+         <div className={styles.usermenu}>
+
+         </div>
+         </> 
+         :
+         ""}
       </div>
       <div className={styles.ad}>
         <div className={styles.grab}>
@@ -38,8 +68,7 @@ export default function Home() {
       <div className={styles.filteringarea}>
         <img src={gridview} alt='' className={styles.grid} />
         <img src={listview} alt='' className={styles.list} />
-        <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRzeZy0fW94mtuxCm2_z0NHZ_FeiD_Domr3_C1WgmzpKMwZO4vxkTr4rwlMXCJ_9cMjOAy5klwEqD_tVJYs4GTvNXYqRo0lfg'
-          style={{ marginLeft: '2vw', width: '30vw', height: '50vh' }} alt='' />
+          
       </div>
       <div className={styles.bottom}>Musicart | All rights reserved</div>
     </>
